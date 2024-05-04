4fa87e63dd7f98c2c7313345743d8d70b6759f65
William Rosa <williamrosaherrera@gmail.com>
2024-05-04 00:05:29 -0600

    creacion de estilo del login

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,76 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <form className="flex flex-col w-full max-w-sm mx-auto mt-8 p-4 border border-gray-300 rounded-lg shadow-md">
-      
-      <label className="mb-2 font-semibold" htmlFor="username">Usuario</label>
-      <input
-        type="text"
-        id="username"
-        className="mb-4 p-2 border border-gray-300 rounded-md"
-        required
-      />
+  return (    
+    <div className="min-h-screen flex justify-center items-center">
+      {/*<div className="min-h-screen bg-white flex justify-center items-center"> para poner el fondo blanco*/}
+    <form className="flex flex-col w-full max-w-sm mx-auto mt-8 p-4 border border-gray-300 rounded-lg shadow-md">      
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-0 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-21 w-auto"
+            src="logo.png"
+            alt="Mrs Paquetes"
+          />
+          <h3 className=" text-center text-2xl font-bold leading-3 tracking-tight text-blue-600">
+            Ingrese sus credenciales
+          </h3>
+        </div>
 
-      <label className="mb-2 font-semibold" htmlFor="password">Contraseña</label>
-      <input
-        type="password"
-        id="password"
-        className="mb-4 p-2 border border-gray-300 rounded-md"
-        required
-      />
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900 ">
+                Correo electronico
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
-      <button
-        type="submit"
-        className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition-colors"
-      >
-        Iniciar sesión
-      </button>
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Contraseña
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-700">
+                    Olvidastes tu contraseña?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Iniciar Sesión
+              </button>
+            </div>
+          </form>        
+        </div>
+      </div>
     </form>
+    </div>
   );
   
 }
